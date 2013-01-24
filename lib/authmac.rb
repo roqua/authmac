981e1b408448339d3de1ffb8624ed4831d5aeac8
Marten Veldthuis <marten@veldthuis.com>
2013-01-24 15:28:07 +0100

    initial working version

--- lib/authmac.rb
+++ lib/authmac.rb
@@ -1,5 +1,33 @@
+require 'openssl'
 require "authmac/version"
 
 module Authmac
-  # Your code goes here...
+  class Authenticator
+    def initialize(secret, digest_function = "sha1")
+      @secret = secret
+      @digest = digest_function
+    end
+
+    def validate(hash, given_hmac)
+      calculate_hmac(hash) == given_hmac
+    end
+
+    private
+
+    def digester
+      OpenSSL::Digest::Digest.new(@digest)
+    end
+
+    def calculate_hmac(hash)
+      OpenSSL::HMAC.hexdigest(digester, @secret, message_string(hash))
+    end
+
+    def message_string(hash)
+      hash_values_sorted_by_key(hash).join('|')
+    end
+
+    def hash_values_sorted_by_key(hash)
+      hash.sort_by {|key, value| key }.map(&:last)
+    end
+  end
 end
--- spec/authmac_spec.rb
+++ spec/authmac_spec.rb
@@ -1,3 +1,47 @@
-describe Authmac do
-  it 'needs implementation'
+require 'authmac'
+
+module Authmac
+  describe Authenticator do
+    let(:authenticator) { Authenticator.new("very secret key", "sha1") }
+
+    context 'for an empty hash' do
+      let(:hash) { Hash.new }
+
+      it 'succeeds with the correct hmac' do
+        authenticator.validate(hash, "d9e74ca3b866d3f563180e9a478c4f9598f29414").should be_true
+      end
+
+      it 'fails with an incorrect hmac' do
+        authenticator.validate(hash, "incorrect hmac").should be_false
+      end
+    end
+
+    context 'for a hash with a single parameter' do
+      it 'succeeds with the correct hmac' do
+        authenticator.validate({single: 'parameter'}, hmacify("parameter")).should be_true
+      end
+
+      it 'fails with incorrect hmac' do
+        authenticator.validate({single: 'parameter'}, 'wrong').should be_false
+      end
+    end
+
+    context 'for a hash with multiple parameters' do
+      it 'succeeds with correct hmac' do
+        authenticator.validate({first: 'parameter', second: 'another'},
+                               hmacify('parameter|another')).should be_true
+      end
+
+      it 'sorts hash values based on their keys' do
+        authenticator.validate({second: 'another', first: 'parameter'},
+                               hmacify('parameter|another')).should be_true
+
+      end
+    end
+
+    def hmacify(string, method='sha1')
+      digester = OpenSSL::Digest::Digest.new(method)
+      OpenSSL::HMAC.hexdigest(digester, "very secret key", string)
+    end
+  end
 end
