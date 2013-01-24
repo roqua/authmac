abee3b8b090b17dc7be9a050bbaeaef2448d4696
Marten Veldthuis <marten@veldthuis.com>
2013-01-24 17:08:11 +0100

    extract methods

--- lib/authmac.rb
+++ lib/authmac.rb
@@ -12,13 +12,22 @@ module Authmac
     end
 
     def validate!(params)
-      hash, hmac = split_params(params)
-      raise HmacError      unless @hmac_checker.validate(hash, hmac)
-      raise TimestampError unless @timestamp_checker.validate(params[:timestamp])
+      validate_hmac!(params)
+      validate_timestamp!(params)
       true
     end
 
     private
+
+    def validate_hmac!(params)
+      hash, hmac = split_params(params)
+      raise HmacError unless @hmac_checker.validate(hash, hmac)
+    end
+
+    def validate_timestamp!(params)
+      timestamp = params[:timestamp]
+      raise TimestampError unless @timestamp_checker.validate(timestamp)
+    end
 
     def split_params(params)
       hash = params.dup
