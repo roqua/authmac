e1de786d9bd039eb96e608442591bef7fdd5a37f
Marten Veldthuis <marten@veldthuis.com>
2013-01-24 17:10:19 +0100

    put inside describe for method name, rename params to hash for consistency with code

--- spec/authmac_spec.rb
+++ spec/authmac_spec.rb
@@ -6,27 +6,29 @@ module Authmac
     let(:timestamp_checker) { stub(validate: true) }
     let(:auth) { Authenticator.new(hmac_checker, timestamp_checker) }
 
-    it 'checks hmac' do
-      params = Hash.new(userid: 'someone', clientid: 'something')
-      hmac   = "a-calculated-hmac"
-      hmac_checker.should_receive(:validate).with(params, hmac)
-      auth.validate!(params.merge(hmac: hmac))
-    end
+    describe '#validate!' do
+      it 'checks hmac' do
+        hash = Hash.new(userid: 'someone', clientid: 'something')
+        hmac = "a-calculated-hmac"
+        hmac_checker.should_receive(:validate).with(hash, hmac)
+        auth.validate!(hash.merge(hmac: hmac))
+      end
 
-    it 'checks timestamp' do
-      timestamp = stub
-      timestamp_checker.should_receive(:validate).with(timestamp)
-      auth.validate!({timestamp: timestamp})
-    end
+      it 'raises HmacError if hmac is incorrect' do
+        hmac_checker.stub(validate: false)
+        expect { auth.validate!({}) }.to raise_error(HmacError)
+      end
 
-    it 'raises HmacError if hmac is incorrect' do
-      hmac_checker.stub(validate: false)
-      expect { auth.validate!({}) }.to raise_error(HmacError)
-    end
+      it 'checks timestamp' do
+        timestamp = stub
+        timestamp_checker.should_receive(:validate).with(timestamp)
+        auth.validate!({timestamp: timestamp})
+      end
 
-    it 'raises TimestampError if timestamp is out of bounds' do
-      timestamp_checker.stub(validate: false)
-      expect { auth.validate!({}) }.to raise_error(TimestampError)
+      it 'raises TimestampError if timestamp is out of bounds' do
+        timestamp_checker.stub(validate: false)
+        expect { auth.validate!({}) }.to raise_error(TimestampError)
+      end
     end
   end
 end
