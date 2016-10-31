4b32eeaf052f9d5b1f1b2eaed2bedcc0cc75bf19
Henk <henk.van.der.veen@gmail.com>
2016-10-31 11:19:32 +0100

    allow json format for message-string to sign

--- README.md
+++ README.md
@@ -21,7 +21,57 @@ Or install it yourself as:
 
 ## Usage
 
-TODO: Write usage instructions here
+```
+checker = Authmac::HmacChecker.new(secret, parameter_separator: '|', digest_function: 'sha1', message_format: :json)
+checker.with_signature(params_to_send)
+```
+
+* message_format: :values or :json. Format of message-string to sign. default :values.
+* digest_function: string to give `OpenSSL::Digest.new(digest_function)`
+* parameter_separator: Used for :values format to separate values.
+
+## Examples
+
+
+```
+params_to_sign = {
+  'some_param' => 'foo',
+  'other_param' => 'bar',
+  'timestamp'    => timestamp.to_s, # 12345678
+  'nonce'        => SecureRandom.urlsafe_base64(4), # ILFtHg
+  'consumer_key' => 'my_key'
+}
+```
+
+Classic syntax: signs values, sorted by key, joined by '|'
+
+```
+checker = Authmac::HmacChecker.new(ENV['consumer_secret'],
+							       parameter_separator: '|',
+							       digest_function: 'sha256')
+params_to_send = checker.with_signature(params_to_sign)
+
+checker.send(:message_string, params_to_sign) # "my_key|ILFtHg|bar|foo|123456789"
+```
+
+Syntax for more complex parameters, signs json, sorted by key (subhashes as well).
+
+```
+params_to_sign['some_param'] = {foo: [1, {bar: 2}]}
+```
+
+```
+checker = Authmac::HmacChecker.new(ENV['consumer_secret'],
+								   digest_function: 'sha256',
+								   message_format: :json)
+params_to_send = checker.with_signature(params_to_sign)
+
+checker.send(:message_string, params_to_sign)
+# '{"consumer_key":"my_key","nonce":"ILFtHg","other_params":"bar",' \
+# '"some_param":{"foo":["1",{"bar":"2"}]},"timestamp":"12345678"}'
+
+```
+
 
 ## Contributing
 
--- spec/authmac/hmac_checker_spec.rb
+++ spec/authmac/hmac_checker_spec.rb
@@ -2,11 +2,11 @@ require 'authmac/hmac_checker'
 
 module Authmac
   describe HmacChecker do
-    let(:checker) { HmacChecker.new('very secret random key of sufficient size', '|', 'sha1') }
+    let(:checker) { HmacChecker.new('very secret random key of sufficient size') }
 
     it 'raises an error for a secret shorter than the hmac output' do
       expect {
-        HmacChecker.new('way too short key', '|', 'sha1')
+        HmacChecker.new('way too short key')
       }.to raise_error SecretError, 'secret too short, see rfc2104'
     end
 
@@ -46,27 +46,39 @@ module Authmac
         end
       end
 
-      context 'for a hash with nested hashes' do
-        it 'succeeds with correct hmac' do
-          expect(checker.validate({first: '1', second: {a: '2', b: '3'}, third: '4'},
-                                  hmacify('1|2|3|4'))).to be_truthy
+      context 'with json message_string' do
+        let(:checker) { HmacChecker.new('very secret random key of sufficient size', message_format: :json) }
+
+        it 'creates a sorted json string' do
+          hash = {first: '1', third: '6', second: {b: ['3', {c: '4', d: '5'}], a: '2'}}
+          expect(checker.send :message_string, hash)
+            .to eq '{"first":"1","second":{"a":"2","b":["3",{"c":"4","d":"5"}]},"third":"6"}'
         end
 
-        it 'sorts hash values based on their keys' do
-          expect(checker.validate({first: '1', third: '4', second: {b: '3', a: '2'}},
-                                  hmacify('1|2|3|4'))).to be_truthy
-        end
-      end
+        context 'for a hash with nested hashes' do
+          it 'succeeds with correct hmac' do
+            expect(checker.validate({first: '1', second: {a: '2', b: '3'}, third: '4'},
+                                    hmacify('{"first":"1","second":{"a":"2","b":"3"},"third":"4"}'))).to be_truthy
+          end
 
-      context 'for a hash with nested hashes and arrays' do
-        it 'succeeds with correct hmac' do
-          expect(checker.validate({first: '1', second: {a: '2', b: ['3', '4']}, third: '5'},
-                                  hmacify('1|2|3|4|5'))).to be_truthy
+          it 'sorts hash values based on their keys' do
+            expect(checker.validate({first: '1', third: '4', second: {b: '3', a: '2'}},
+                                    hmacify('{"first":"1","second":{"a":"2","b":"3"},"third":"4"}'))).to be_truthy
+          end
         end
 
-        it 'sorts hash values based on their keys' do
-          expect(checker.validate({first: '1', third: '6', second: {b: ['3', {c: '4', d: '5'}], a: '2'}},
-                                  hmacify('1|2|3|4|5|6'))).to be_truthy
+        context 'for a hash with nested hashes and arrays' do
+          it 'succeeds with correct hmac' do
+            expect(checker.validate({first: '1', second: {a: '2', b: ['3', '4']}, third: '5'},
+                                    hmacify('{"first":"1","second":{"a":"2","b":["3","4"]},"third":"5"}')))
+                          .to be_truthy
+          end
+
+          it 'sorts hash values based on their keys' do
+            expect(checker.validate({first: '1', third: '6', second: {b: ['3', {c: '4', d: '5'}], a: '2'}},
+                                    hmacify( '{"first":"1","second":{"a":"2","b":["3",{"c":"4","d":"5"}]},"third":"6"}')))
+                          .to be_truthy
+          end
         end
       end
     end
